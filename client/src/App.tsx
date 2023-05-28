4dd98a48c6d43c8c1ce81d9d9613276298340030
AdneCode <super-gijs@hotmail.com>
2023-05-28 18:15:13 +0200

    genesis

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="h-screen w-full bg-red-500">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
