4190b01f555509e00a2552585536f770333c09f2
AdneCode <super-gijs@hotmail.com>
2023-05-29 11:27:15 +0200

    feat: playerDiv

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css';
 
+import { PlayerDiv } from './PlayerDiv';
 import React from 'react';
 import logo from './logo.svg';
 
 function App() {
   return (
     <div className="h-screen w-full bg-red-500">
-      <div></div>
+      <PlayerDiv id={1} />
       <div></div>
       <div></div>
     </div>
---
+++ client/src/PlayerDiv.tsx
@@ -0,0 +1,5 @@
+export const PlayerDiv = ({id } : {id: number}) => {
+    return (
+        <div className="w-full bg-blue-300" style={{height: '10vh'}}></div>
+    )
+    }
