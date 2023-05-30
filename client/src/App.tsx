aec1b77c44add3bef07bb57383c557a376ffdb4f
AdneCode <super-gijs@hotmail.com>
2023-05-30 20:41:24 +0200

    sort: commentary

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,27 +15,12 @@ function App() {
     const other = useOther();
     const dispatch = useDispatch();
     const socket = useSocket();
-
-    // if (!self?.id) {
-    //     return <QRCode id={getId(5)} />;
-    // }
-    console.log(self);
-    console.log(other);
-    console.log(self);
     return (
         <>
             <SocketReceiver />
             <Routes>
                 <Route path="/qr" element=<QRCode /> />
                 <Route path="/twopine/:id" element=<Other /> />
-                {/* <div
-                className="h-screen w-full bg-red-500"
-                onMouseUp={() => dispatch(unclick())}
-                onMouseMove={(e: React.MouseEvent) => handleMouseMove(e)}>
-                <PlayerDiv id={1} self={self} other={other} />
-                <div></div>
-                <div></div>
-            </div> */}
             </Routes>
         </>
     );
